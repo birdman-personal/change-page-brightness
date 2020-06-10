2009c075851f982e8eff4d32b279453284f2f258
Victor Tsay <vt0826@hotmail.com>
2020-06-10 15:49:31 -0400

    add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const rangeInput = document.getElementById("range");
+  const container = document.getElementsByClassName("container")[0];
+  rangeInput.addEventListener("change", function () {
+    container.style.filter = `brightness(${rangeInput.value}%)`;
+  });
+});
